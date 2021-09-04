2790d57ba9df65239b36214a4ffa01b3e69b844f
Jamil Pinheiro <rafaeljamil@gmail.com>
2021-09-04 02:06:14 -0300

    Commit de modules, profiles e sites

---
+++ desafio/desafio-stefanini/sites/default/files/php/twig/6132c1cbeb614___string_template__537da4_6rFKYk7cx0CnWj1mbgZmg9-ji/3uZDhEn4_8j9OUGkavia4IxShEkd5rOFZSB3FpNsoCc.php
@@ -0,0 +1,80 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__537da49d8777bf77494e1f86ec4a8727863803c7335e08fb658fcdf0d88e5fdb */
+class __TwigTemplate_f9dd2732723b0614761f50b767752f6f3f3d7e8884a8ca1152ce0bbec8fac2e7 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = [];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<h1>Desculpe, não há mais vagas para este evento! </h1>";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__537da49d8777bf77494e1f86ec4a8727863803c7335e08fb658fcdf0d88e5fdb";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__537da49d8777bf77494e1f86ec4a8727863803c7335e08fb658fcdf0d88e5fdb", "");
+    }
+}
